907622d94259e43a078c2a3e604d4b8643a96bba
Muhammad Azeem <mailmeatazeem@gmail.com>
2024-06-20 16:30:15 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 // Code 
 
-// Definition for a binary tree node.
+// Definition for singly-linked list.
 /*
-struct TreeNode 
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 */
+
 class Solution {
 public:
 
